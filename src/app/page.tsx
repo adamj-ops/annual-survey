f2845c23f0d441faee4cf4666aee57578ac2e815
adamj-ops <adam@opsfx.io>
2025-11-14 12:39:19 -0600

    feat: center survey content vertically in viewport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { MultiStepSurvey } from "@/components/survey/MultiStepSurvey"
 export default function Home() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
-      <div className="container mx-auto px-4 pt-6 pb-8 flex-1 flex flex-col items-center">
+      <div className="container mx-auto px-4 py-6 flex-1 flex flex-col items-center justify-center">
         {/* Centered form content */}
-        <div className="flex-1 flex flex-col justify-start pt-8 w-full max-w-4xl">
+        <div className="w-full max-w-4xl">
           <MultiStepSurvey />
         </div>
       </div>
